f6661aa465caa8d22c3ede8cb1bccd0d85580fde
David Fudge <rkstar@mac.com>
2017-08-16 01:19:06 -0400

    add a space to each "block" to allow for accurate regex search of numbered indexes regardless of character count.
    incorporate originIndex into moveBlocksToPosition to force our origin -> destination block indexes to be explicit

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-const initState = n => Array(n).fill([], 0, n).map((block, i) => `${i}`);
+const existsInString = (block, n) => {
+  const rx = new RegExp('(^|\\s)'+n+'(\\s|$)');
+  return block.search(rx) > -1;
+};
+const initState = n => Array(n).fill([], 0, n).map((block, i) => `${i} `);
 module.exports.initState = initState;
 
 const validMove = (state, { a, b }) => notEqual(a, b) && notInSameStack(state, { a, b });
 const notEqual = (a, b) => a !== b;
-const notInSameStack = (state, { a, b }) => state.reduce((response, stack) => stack.includes(a) && stack.includes(b) ? false : response, true);
+const notInSameStack = (state, { a, b }) => state.reduce((response, stack) => existsInString(stack, a) && existsInString(stack, b) ? false : response, true);
 
-const stackIndex = (state, n) => state.reduce((index, block, i) => block.includes(n) ? i : index, 0);
-const blocksOnTop = (stack, n) => stack.slice(stack.indexOf(n) + 1);
+const stackIndex = (state, n) => state.reduce((index, block, i) => existsInString(block, n) ? i : index, 0);
+const blocksOnTop = (stack, n) => stack.slice(stack.indexOf(n) + 2);
 
-const returnBlocksToPosition = (state, blocks) => state.map((stack, i) => blocks.includes(i) ? `${i}` : stack);
-const moveBlocksToPosition = (state, { from, to }) => {
+const returnBlocksToPosition = (state, blocks) => state.map((stack, i) => existsInString(blocks, i) ? `${i} ` : stack);
+const moveBlocksToPosition = (state, { originIndex, from, to }) => {
   const newState = [ ...state ];
-
-  newState[from.toString().charAt(0)] = '';
-  newState[to] += from;
+  newState[originIndex] = '';
+  newState[to] += `${from} `;
   return newState;
 };
 
@@ -25,7 +28,7 @@ const returnStackedBlocks = (state, { n, append = false }) => {
   if (blocksOnTopOfN.length) {
     // remove blocks from the stack a is in
     newState[stackIndexOfN] = newState[stackIndexOfN].split(n)[0];
-    newState[stackIndexOfN] += append ? n : '';
+    newState[stackIndexOfN] += append ? `${n} ` : '';
     // move the blocks on top of a back to their origins
     newState = returnBlocksToPosition(newState, blocksOnTopOfN);
   }
@@ -41,7 +44,7 @@ const moveOnto = (state, { a, b }) => {
   let newState = [ ...state ];
   newState = returnStackedBlocks(newState, { n: a });
   newState = returnStackedBlocks(newState, { n: b, append: true });
-  return moveBlocksToPosition(newState, { from: a, to: stackIndex(newState, b) });
+  return moveBlocksToPosition(newState, { originIndex: a, from: a, to: stackIndex(newState, b) });
 };
 module.exports.moveOnto = moveOnto;
 
@@ -52,11 +55,11 @@ const moveOver = (state, { a, b }) => {
 
   let newState = [ ...state ];
   newState = returnStackedBlocks(newState, { n: a });
-  return moveBlocksToPosition(newState, { from: a, to: stackIndex(newState, b) });
+  return moveBlocksToPosition(newState, { originIndex: a, from: a, to: stackIndex(newState, b) });
 };
 module.exports.moveOver = moveOver;
 
-const pileOnto = (state, {a, b}) => {
+const pileOnto = (state, { a, b }) => {
   if (!validMove(state, { a, b })) {
     return state;
   }
@@ -64,16 +67,16 @@ const pileOnto = (state, {a, b}) => {
   let newState = [ ...state ];
 
   const stackIndexOfA = stackIndex(newState, a);
-  const stackedBlocksA = `${a}${blocksOnTop(newState[stackIndexOfA], a)}`;
+  const stackedBlocksA = `${a} ${blocksOnTop(newState[stackIndexOfA], a)}`;
   // remove stackedBlocksA from stack that they are currently on
   newState[stackIndexOfA] = newState[stackIndexOfA].split(a)[0];
 
   newState = returnStackedBlocks(newState, { n: b, append: true });
-  return moveBlocksToPosition(newState, { from: stackedBlocksA, to: stackIndex(newState, b) });
+  return moveBlocksToPosition(newState, { originIndex: a, from: stackedBlocksA, to: stackIndex(newState, b) });
 };
 module.exports.pileOnto = pileOnto;
 
-const pileOver = (state, {a, b}) => {
+const pileOver = (state, { a, b }) => {
   if (!validMove(state, { a, b })) {
     return state;
   }
@@ -81,18 +84,19 @@ const pileOver = (state, {a, b}) => {
   let newState = [ ...state ];
 
   const stackIndexOfA = stackIndex(newState, a);
-  const stackedBlocksA = `${a}${blocksOnTop(newState[stackIndexOfA], a)}`;
+  const stackedBlocksA = `${a} ${blocksOnTop(newState[stackIndexOfA], a)}`;
   // remove stackedBlocksA from stack that they are currently on
   newState[stackIndexOfA] = newState[stackIndexOfA].split(a)[0];
 
   // pile the stack of a on top of b!
-  return moveBlocksToPosition(newState, { from: stackedBlocksA, to: stackIndex(newState, b) });
+  return moveBlocksToPosition(newState, { originIndex: a, from: stackedBlocksA, to: stackIndex(newState, b) });
 };
 module.exports.pileOver = pileOver;
 
 const quit = state => {
   // console.log(state);
-  state.map((block, i) => console.log(`${i}: ${block.trim().split('').join(' ')}`));
+  state.reduce((s, block) => block === ' ' ? s : s.concat(block), [])
+    .map((block, i) => console.log(`${i}: ${block.trim()}`));
   process.exit();
 };
 module.exports.quit = quit;
